041d20cb5593e9c73719612096e938324e885b82
Ajay <ajaystefins@gmail.com>
2025-12-18 21:29:57 +0530

    ff

--- lib/models/request/project/contract_comment.dart
+++
@@ -1,6 +0,0 @@
-class ContractComment {
-  final String text;
-  final DateTime timestamp;
-
-  ContractComment({required this.text, required this.timestamp});
-}
--- lib/repositories/template_repository.dart
+++
@@ -1,4 +0,0 @@
-class TemplateRepository {
-  static final _singleton = TemplateRepository();
-  static TemplateRepository get instance => _singleton;
-}
